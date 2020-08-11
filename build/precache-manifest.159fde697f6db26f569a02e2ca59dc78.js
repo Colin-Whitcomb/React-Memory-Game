73e1e8fef2ac50cb44adae9add0fb3a6c2a85be2
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-11 16:41:39 -0700

    [add] to GH Pages.

---
+++ build/precache-manifest.159fde697f6db26f569a02e2ca59dc78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71e8ec87cffb8d0bfccff9004a8c831",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "0fb91788708921a12266",
+    "url": "/React-Memory-Game/static/css/main.44a8476c.chunk.css"
+  },
+  {
+    "revision": "4cb0ff2b6213b3374c6c",
+    "url": "/React-Memory-Game/static/js/2.9a2ca08a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Memory-Game/static/js/2.9a2ca08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb91788708921a12266",
+    "url": "/React-Memory-Game/static/js/main.4c8ceeda.chunk.js"
+  },
+  {
+    "revision": "b5c206fddb84822872e9",
+    "url": "/React-Memory-Game/static/js/runtime-main.16b400c4.js"
+  }
+]);
