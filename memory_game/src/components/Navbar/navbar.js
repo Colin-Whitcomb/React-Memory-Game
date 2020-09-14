457a81c04d2512e9fe2f98d29fb876406c850dd5
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-14 12:27:31 -0700

    [delete] link in navbar.

--- memory_game/src/components/Navbar/navbar.js
+++ memory_game/src/components/Navbar/navbar.js
@@ -3,7 +3,7 @@ import React from "react";
     function navbar (props) {
         return (
         <nav className="navbar navbar-dark bg-dark">
-             <a className="navbar-brand" href="">React Game</a>
+             <h4 className="navbar-brand">React Game</h4>
              {props.message ? (<h4 className="navbar-brand" >{props.message}</h4>) : (<h4 className="navbar-brand" >Be careful not to pick the same Avatar!</h4>)}
              {/* <h4 className="navbar-brand" >Score: {props.count} | High Score {props.highScore}</h4> */}
       </nav>
